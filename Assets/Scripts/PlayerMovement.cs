ed8cd045a0e4a33680200ed640799f9268bf989e
PhavalH <60720443+PhavalH@users.noreply.github.com>
2020-05-20 19:49:24 -0400

    Final Project UPDATE
    Added point system

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,63 +11,69 @@ public class PlayerMovement : MonoBehaviour
     public float tapForce = 275; //upward force
     public float tiltSmooth = 10;
     public Vector2 startPos;
-    public float pointValue; 
+    public float pointValue;
+
+    public GameOperator gameOperatorInstance;
 
     public ScoreScript scoreScriptInstance;
+    public Animator motionLayer;
     
-    private Rigidbody2D rigidbody;
+    Rigidbody2D rigidbody;
 
     
     //rotates Bird
     Quaternion downRotation;
     Quaternion forwardRotation;
+
     
 
     void Start()
     {
-        
-
         //Get and store a reference to the Rigidbody2D attached to this GameObject.
         rigidbody = GetComponent<Rigidbody2D>();
+
         //Returns a rotation that rotates z degrees around the z axis, 
         //x degrees around the x axis, and y degrees around the y axis;
         downRotation = Quaternion.Euler(0, 0, -90);
         forwardRotation = Quaternion.Euler(0, 0, 35);
+        
     }
 
     void Update()
     {
-        
-        
-            //applies movement to the player
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                transform.rotation = forwardRotation;
-                rigidbody.velocity = Vector2.zero;
-                rigidbody.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
-            }
-
-            transform.rotation = Quaternion.Lerp(transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
-
-        //triggers dead zone & score zone
-        void OnCollisionEnter2D (Collider2D col)
+        //applies movement to the player
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            //scoreScript.pointValue += 10; 
-            //scoreScriptInstance.gamescore;
-            //scoreScriptInstance.gamePoints;
-            
-            if (col.gameObject.tag == "ScoreZone")
-            {
-                ScoreScript.pointValue += 5; 
-            }
-
-            if (col.gameObject.tag == "Deadzone")
-            {
-                //Don't allow control if the bird has died.
-                rigidbody.simulated = false;
-            }
-
+            transform.rotation = forwardRotation;
+            rigidbody.velocity = Vector2.zero;
+            rigidbody.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
         }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
+
+    }
+
+    //triggers dead zone & score zone
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("Touched A Collider " + col.gameObject.tag);
+        
+
+        if (col.gameObject.CompareTag("Scorezone"))
+        {
+            scoreScriptInstance.pointValue += 5; //adds points when the player triggers the scorezone
+        }
+
+        if (col.gameObject.CompareTag("Deadzone"))
+        {
+            //Don't allow control if the bird has died.
+            rigidbody.simulated = false;
+            Debug.Log("Deadzone Touched");
+            gameOperatorInstance.SetPageState(GameOperator.PageState.GameOver);  //refers to page state in game operator script
+            motionLayer.enabled = false; //stops animation
+            
+        }
+
     }
 }
 
--- Assets/Scripts/ScoreScript.cs
+++ Assets/Scripts/ScoreScript.cs
@@ -4,25 +4,25 @@ using System.Globalization;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+//this script is the game's point system
 public class ScoreScript : MonoBehaviour
 {
 
+    public int pointValue = 0;
 
-    public static int pointValue = 0;
-    Text gamescore;
-    
-    
+    public Text gamescore;
+
     // Start is called before the first frame update
     void Start()
     {
-        gamescore = GetComponent<Text> ();
+        gamescore.text = "Score: " + pointValue.ToString();  //adds points
+        
     }
 
     // Update is called once per frame
     public void Update()
     {
-        gamescore.text = "Score: " + pointValue;
+        gamescore.text = "Score: " + pointValue.ToString();  //adds points
     }
-
-
 }
